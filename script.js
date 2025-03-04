4cec633baffe8d95ed3da6ed4fcc53f9ca282951
Erikiriggs <erickmahindi@gmail.com>
2025-03-04 13:42:36 +0300

    new file:   Assets/Prado1.jpg 	new file:   Assets/Raptor.jpeg 	modified:   Home.html 	modified:   MainPages/AboutUs.html 	modified:   MainPages/Contact.html 	new file:   MainPages/Reviews.html 	modified:   MainPages/product.html 	deleted:    index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,30 +1,5 @@
-document.getElementById("loginForm").addEventListener("submit", function(event) {
-  event.preventDefault(); // Prevent form from submitting normally
-
-  let username = document.getElementById("userName").value;
-  let password = document.getElementById("password").value;
-
-  const validUsername = "admin";
-  const validPassword = "password123";
-
-  if (username === "" || password === "") {
-      alert("Both fields must be filled out.");
-      return;
-  }
-
-  if(username === validUsername && password ===validPassword){
-    console.log('Login Successful');
-    localStorage.setItem("loggedIn", "true");
-    // Redirect to home page after successful login
-    window.location.href = "Home.html";
-  }
-  else{
-    alert("Invalid Username or Password");
-  }
-
-  console.log("Username:", username);
-  console.log("Password:", password);
-
+document.getElementById("toProducts").addEventListener("click", function(){
+  window.location.href='MainPages/product.html'
 });
 
 
